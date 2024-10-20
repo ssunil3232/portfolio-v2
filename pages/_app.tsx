3b9aeb97be992cd06c117b372628c1b46806afd4
ssunil3232 <soniasunil47@gmail.com>
2024-10-20 11:34:58 -0400

    update yellow bricks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import { nunito } from '@/app/ui/fonts';
 import '../src/app/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
+      <>
+      <Head>
+        <title>Sonia Sunil - Frontend Developer | UX Designer</title>
+        <meta
+          name="description"
+          content="Portfolio website of all my projects"
+        />
+      </Head>
         <div className={nunito.className}>
           <Component {...pageProps} />
         </div>
+    </>
+        
       );
 }
 
